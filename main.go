0a1ccf6b431bee7ce94bcdb4b88c2dc1a938ea69
Taro Kambara <taro_kambara@unipulse.co.jp>
2021-07-01 15:57:38 +0900

    i

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var dict = map[int]string{
+	int('T'): "🀀",
+	int('N'): "🀁",
+	int('S'): "🀂",
+	int('P'): "🀃",
+}
+
+func init() {
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("asdf")
+	err := run()
+	if err != nil {
+		log.Print(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var filename string
+
+	if args := flag.Args(); len(args) > 0 {
+		filename = args[0]
+	}
+
+	var r io.Reader
+	switch filename {
+	case "", "-":
+		r = os.Stdin
+	default:
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		r = f
+	}
+	t, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	// fmt.Print(string(t))
+
+	for _, char := range string(t) {
+		fmt.Print(dict[int(char)])
+	}
+
+	return nil
 }
